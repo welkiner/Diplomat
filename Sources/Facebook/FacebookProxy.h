01505be05d625b168d46c614dcde5c6f2f4f1329
tl <tl@iMac.local>
2017-06-02 09:49:45 +0800

    add facebook sdk

---
+++ Sources/Facebook/FacebookProxy.h
@@ -0,0 +1,15 @@
+//
+//  FacebookProxy.h
+//  Diplomat
+//
+//  Created by tl on 01/06/2017.
+//  Copyright © 2017 Cloud Dai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Diplomat.h"
+
+
+@interface FacebookProxy : NSObject
+
+@end
---
+++ Sources/Facebook/FacebookProxy.m
@@ -0,0 +1,13 @@
+//
+//  FacebookProxy.m
+//  Diplomat
+//
+//  Created by tl on 01/06/2017.
+//  Copyright © 2017 Cloud Dai. All rights reserved.
+//
+
+#import "FacebookProxy.h"
+
+@implementation FacebookProxy
+
+@end
